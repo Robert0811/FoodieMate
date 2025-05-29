c5585c961cf7e0d4a3b73dc6cef4bde087c9f9ee
Robert Trifan <trifanrobert55@yahoo.com>
2025-05-29 03:55:45 +0300

    First commit - Initial project setup

---
+++ FoodieMate/FoodieMate/FoodieMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodieMateApp.swift
+//  FoodieMate
+//
+//  Created by Robert Trifan on 29.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct FoodieMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
