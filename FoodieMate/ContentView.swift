59a2439dbbb3901d50520f6a06b8f5acf40a5317
Trifan Robert <trifanrobert55@yahoo.com>
2025-05-29 07:58:16 +0300

    Initial Commit

---
+++ FoodieMate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodieMate
+//
+//  Created by Robert Trifan on 29.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
